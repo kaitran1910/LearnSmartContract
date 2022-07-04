e1b7a41c74f1750da0e1d841f247d8e69537ece0
Tam Nhu Tran <kaitran.ntt@gmail.com>
2022-07-04 14:22:58 -0400

    Updating compile.js - Add the expected JSON formatted input, specifying the language, sources, and outputSelection - Update the export to provide the expected JSON formatted output

--- compile.js
+++ compile.js
@@ -7,5 +7,40 @@ const solc = require("solc");
 const inboxPath = path.resolve(__dirname, "contracts", "Inbox.sol");
 const source = fs.readFileSync(inboxPath, "utf8");
 
-// console.log(solc.compile(source, 1);
-module.exports = solc.compile(source, 1).contracts[":Inbox"];
+const input = {
+    language: "Solidity",
+    sources: {
+        "Inbox.sol": {
+            content: source,
+        },
+    },
+    settings: {
+        outputSelection: {
+            "*": {
+                "*": ["*"],
+            },
+        },
+    },
+};
+
+/**
+ * console.log(JSON.parse(solc.compile(JSON.stringify(input))).contracts);
+ *      will return the following:
+ * {
+    'Inbox.sol': {
+        Inbox: {
+        abi: [Array],
+        devdoc: [Object],
+        evm: [Object],
+        ewasm: [Object],
+        metadata: '{...}',
+        storageLayout: [Object],
+        userdoc: [Object]
+        }
+    }
+    }
+ */
+
+module.exports = JSON.parse(solc.compile(JSON.stringify(input))).contracts[
+    "Inbox.sol"
+].Inbox;
