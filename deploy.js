741b1a18251971c47e7fbf6eb262291c4d299183
Tam Nhu Tran <kaitran.ntt@gmail.com>
2022-07-04 14:42:04 -0400

    Updating deploy.js - Update the import to use the newer @truffle/hdwallet-provider module. - Update the import to destructure the abi (formerly the interface) and the evm (bytecode) 	const { abi, evm } = require('./compile'); - Pass the abi to the contract object: 	const result = await new web3.eth.Contract(abi) - Assign the bytecode to the data property of the deploy method:     .deploy({       data: evm.bytecode.object,

--- deploy.js
+++ deploy.js
@@ -4,7 +4,8 @@
  */
 const HDWalletProvider = require("@truffle/hdwallet-provider");
 const Web3 = require("web3");
-const { interface, bytecode } = require("./compile");
+
+const { abi, evm } = require("./compile");
 
 /**
  ** Remember to duplicate the mnemonic.js.sample and
@@ -25,12 +26,12 @@ const INITIAL_MESSAGE = "Hi there!";
  * wrap the code in a function
  */
 const deploy = async () => {
-    const accounts = await web3.eth.getAccounts();
+    accounts = await web3.eth.getAccounts();
 
     console.log("Attempting to deploy from account", accounts[0]);
 
-    inbox = await new web3.eth.Contract(JSON.parse(interface))
-        .deploy({ data: bytecode, arguments: [INITIAL_MESSAGE] })
+    inbox = await new web3.eth.Contract(abi)
+        .deploy({ data: evm.bytecode.object, arguments: [INITIAL_MESSAGE] })
         .send({ from: accounts[0], gas: "1000000" });
 
     console.log("Contract deployed to: ", inbox.options.address);
