975cd0b377ca33700cec9a66b1d59fd34b5fc76a
Tam Nhu Tran <kaitran.ntt@gmail.com>
2022-07-04 14:28:35 -0400

    Updating Inbox.test.js - Update the import to destructure the abi (formerly the interface) and the evm (bytecode) - Pass the abi to the contract object - Assign the bytecode to the data property of the deploy method:     .deploy({       data: evm.bytecode.object,

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,31 +1,31 @@
 /**
  * Modules usage explanation:
- * 
+ *
  * - "assert": For making assertions about the test,
  * mostly used to assert some value is equal to another
- * 
+ *
  * - "ganache-cli": for deploying a local test network
  * do pretty much the same thing as using Remix Editor
- * 
+ *
  * - "web3": a library for interacting with the test network
  * (here only use web3 constructor, hence using a capital Web3)
  * (web3 only supports promises + async/await since v1.*.*)
- * 
- * - provider(): a communication layer between web3 library 
- * and some specific ETH network, which is Ganache (the 
+ *
+ * - provider(): a communication layer between web3 library
+ * and some specific ETH network, which is Ganache (the
  * local test network) in this case
- * 
- * - interface: the ABI of the contract, which is the
+ *
+ * - abi: the ABI of the contract, which is the
  * interface between the contract and the test network
- * 
- * - bytecode: the bytecode of the contract, which is
+ *
+ * - evm: the bytecode of the contract, which is
  * the compiled code of the contract
  */
 const assert = require("assert");
 const ganache = require("ganache-cli");
 const Web3 = require("web3");
 const web3 = new Web3(ganache.provider());
-const { interface, bytecode } = require("../compile");
+const { abi, evm } = require("../compile");
 
 /**
  * Mocha functions:
@@ -44,19 +44,25 @@ beforeEach(async () => {
 
     /**
      * Use one of those accounts to deploy the contract
-     * 
-     * - web3.eth.Contract(JSON.parse(interface))
+     *
+     * - new web3.eth.Contract(abi)
      * teach web3 about what methods a Inbox contract should have
-     * 
-     * - deploy({ data: bytecode, arguments: [INITIAL_MESSAGE] })
+     *
+     * - .deploy({
+            data: evm.bytecode.object,
+            arguments: [INITIAL_MESSAGE],
+        })
      * tell web3 that we want to deploy a new copy of this Inbox contract
-     * 
-     * - send({ from: accounts[0], gas: "1000000" })
+     *
+     * - .send({ from: accounts[0], gas: "1000000" })
      * instruct web3 to send out a transaction from the first account
      * to create this contract, with a gas limit of 1000000
      */
-    inbox = await new web3.eth.Contract(JSON.parse(interface))
-        .deploy({ data: bytecode, arguments: [INITIAL_MESSAGE] })
+    inbox = await new web3.eth.Contract(abi)
+        .deploy({
+            data: evm.bytecode.object,
+            arguments: [INITIAL_MESSAGE],
+        })
         .send({ from: accounts[0], gas: "1000000" });
 });
 
